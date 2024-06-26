6de038a30fd27fa7a022e5bb60dfc4180ac78f15
Vijay3626 <vijaysekar3626@gmail.com>
2024-06-26 14:39:35 +0530

    inintial commit

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ export default function Page() {
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black" />
+        hi
       </div>
     </main>
   );
